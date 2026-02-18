bdadffe59f990adc14c3d543dd57ecaffdfc56d4
terrablood <toth.levente.22f@szlgbp.hu>
2026-02-18 11:35:18 +0100

    ledfelkapcsolaskijelentesekesz

---
+++ app_ledkapcsolas/static/hatter.js
@@ -0,0 +1,8 @@
+const on = document.getElementById("felkapcs");
+on.addEventListener("click", function () {
+    alert("led felkapcsolva!");
+});
+const off = document.getElementById("lekapcs");
+off.addEventListener("click", function () {
+    alert("led lekapcsolva!");
+});
